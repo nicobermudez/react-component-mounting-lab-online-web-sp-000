8b6ed1c771a7cda11031f767de616f74b37d22a9
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 14:22:20 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 import Timer from './Timer'
 
 class App extends Component {
-  constructor {
-    //no props being used here, so we can use the shorthand declaration of state
-    state = {
-      timerIDs: []
-    }
+  
+  //no props being used here, so we can use the shorthand declaration of state
+  state = {
+    timerIDs: []
   }
 
-  
+
+  //Your code here:
 
 
 
